a9c216c419ca8b880a1c405bd8a8f33960a920ea
Tanyapond <tanyaponddd@gmail.com>
2021-06-13 11:51:42 +0000

    Add product details functionality and Add 2 new column in products models app

--- products/models.py
+++ products/models.py
@@ -7,6 +7,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    lash_length_and_curl = models.TextField(blank=True)
+    lash_line_length = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField()
 
