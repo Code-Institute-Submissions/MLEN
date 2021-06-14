31fc6d480cbb5ab0ca499a19beada96154e50751
Tanyapond <tanyaponddd@gmail.com>
2021-06-14 18:44:58 +0000

    Add notification css, shopping cart preview and addiontal msg

--- mlen/settings.py
+++ mlen/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-FREE_DELIVERY_THRESHOLD = 200
+FREE_DELIVERY_THRESHOLD = 250
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
