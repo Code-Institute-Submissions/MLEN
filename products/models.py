e38814c424d61a59341f7552cb70744ae035065c
Tanyapond <tanyaponddd@gmail.com>
2021-06-18 16:55:34 +0000

    Change lash product text models and Add admin add product view, url and template

--- products/models.py
+++ products/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Product(models.Model):
     sku = models.CharField(max_length=254, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    lash_length_and_curl = models.TextField(blank=True)
-    lash_line_length = models.TextField(blank=True)
+    lash_length_and_curl = models.CharField(max_length=254)
+    lash_line_length = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField()
 
