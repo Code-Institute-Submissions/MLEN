7bb47d900dd7f7f1d89207864406b7b6aef50466
Tanyapond <tanyaponddd@gmail.com>
2021-06-22 07:42:09 +0000

    Change debug settings

--- mlen/settings.py
+++ mlen/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tanya-mlen.herokuapp.com', 'localhost']
 
